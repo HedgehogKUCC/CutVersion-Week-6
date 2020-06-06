06c74d1463f5128c050c12be59a7def1e6dc0cc5
HedgehogKU <ccc908925@gmail.com>
2020-06-06 18:55:44 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# 第六週版型攻略
+
+[筆記](https://hackmd.io/_NJqIbzwSDGS1-r5mKz2gQ?view)
---
+++ app/assets/js/all.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+  $('.homeSlick').slick({
+    cssEase: 'linear',
+    arrows: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
