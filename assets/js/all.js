7d4b1410e78183b7b92f979b4e4aac30d0bdc130
HedgehogKU <ccc908925@gmail.com>
2020-06-06 18:57:34 +0800

    Initial dist subtree commit

---
+++ assets/js/all.js
@@ -0,0 +1,13 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.homeSlick').slick({
+    cssEase: 'linear',
+    arrows: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+});
+//# sourceMappingURL=all.js.map
